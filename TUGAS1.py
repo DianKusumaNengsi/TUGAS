3d6af0b69d40b464cb7109816cb69ddb63f609dc
DianKusumaNengsi <101321827+DianKusumaNengsi@users.noreply.github.com>
2022-04-14 15:25:54 +0800

    Add files via upload

---
+++ TUGAS1.py
@@ -0,0 +1,87 @@
+print("Dian Kusuma Nengsi")
+print("D0219321")
+
+print('=' * 25)
+print("1. Penjumlahan")
+print("2. Pengurangan")
+print("3. Perkalian")
+print("4. Pembagian")
+print('=' * 25)
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+a= int(input("Bilangan 1 : "))
+b= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = a+b
+elif pil == 2:
+   hasil = a-b
+elif pil == 3:
+   hasil = a*b
+elif pil == 4:
+   hasil = a/b
+
+print("\nHasil : %d" %hasil)
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = a+b
+elif pil == 2:
+   hasil = a-b
+elif pil == 3:
+   hasil = a*b
+elif pil == 4:
+   hasil = a/b
+print("\nHasil : %d" %hasil)
+
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = a+b
+elif pil == 2:
+   hasil = a-b
+elif pil == 3:
+   hasil = a*b
+elif pil == 4:
+   hasil = a/b
+print("\nHasil : %d" %hasil)
+
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = a+b
+elif pil == 2:
+   hasil = a-b
+elif pil == 3:
+   hasil = a*b
+elif pil == 4:
+   hasil = a/b
+print("\nHasil : %d" %hasil)
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = a+b
+elif pil == 2:
+   hasil = a-b
+elif pil == 3:
+   hasil = a*b
+elif pil == 4:
+   hasil = a/b
+print("\nHasil : %d" %hasil)
